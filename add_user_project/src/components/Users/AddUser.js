4bf669bede9ce69dab9410c0acfacb02cb239b19
AishwaryaSB99 <aishwaryasampathsb@gmail.com>
2023-09-03 13:00:26 +0530

    fragments, portals, refs

--- add_user_project/src/components/Users/AddUser.js
+++ add_user_project/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Button from "../UI/Button";
 import Card from "../UI/Card";
@@ -6,29 +6,36 @@ import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-  const initialValues = {
-    username: "",
-    age: "",
-  };
-  const [enteredUserDetails, setEnteredUserDetails] = useState(initialValues);
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+//   const initialValues = {
+//     username: "",
+//     age: "",
+//   };
+  //   const [enteredUserDetails, setEnteredUserDetails] = useState(initialValues);
   const [error, setError] = useState();
 
-  const usernameHandler = (e) => {
-    setEnteredUserDetails((prevState) => {
-      return { ...prevState, username: e.target.value };
-    });
-  };
+  //   const usernameHandler = (e) => {
+  //     setEnteredUserDetails((prevState) => {
+  //       return { ...prevState, username: e.target.value };
+  //     });
+  //   };
 
-  const ageHandler = (e) => {
-    setEnteredUserDetails((prevState) => {
-      return { ...prevState, age: +e.target.value };
-    });
-  };
+  //   const ageHandler = (e) => {
+  //     setEnteredUserDetails((prevState) => {
+  //       return { ...prevState, age: +e.target.value };
+  //     });
+  //   };
   const submitHandler = (e) => {
     e.preventDefault();
+    console.log(nameInputRef);
+    const eneteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
     if (
-      enteredUserDetails.username.toString().trim().length === 0 ||
-      enteredUserDetails.age.toString().trim().length === 0
+      //   enteredUserDetails.username.toString().trim().length === 0 ||
+      //   enteredUserDetails.age.toString().trim().length === 0
+      eneteredName.toString().trim().length === 0 ||
+      enteredUserAge.toString().trim().length === 0 //ref approach to avoid setting values state in every keystroke
     ) {
       setError({
         title: "Invalid input!",
@@ -36,17 +43,27 @@ const AddUser = (props) => {
       });
       return;
     }
-    if (enteredUserDetails.age < 1) {
+    if (
+      // enteredUserDetails.age < 1
+      enteredUserAge < 1
+    ) {
       setError({
         title: "Invalid age!",
         message: "Please enter valid a age (> 0)",
       });
       return;
     }
-    props.onAddUser(enteredUserDetails.username, enteredUserDetails.age);
-    setEnteredUserDetails(initialValues);
+    props.onAddUser(
+      // enteredUserDetails.username, enteredUserDetails.age
+      eneteredName,
+      enteredUserAge
+    );
+    nameInputRef.current.value=''//Reset logic, we should avoid manipulating DOM usually but in
+                                // this case it's only resetting value, so using this approach 
+    ageInputRef.current.value=''
+    // setEnteredUserDetails(initialValues);
     setError(false);
-    console.log(enteredUserDetails);
+    // console.log(enteredUserDetails);
   };
 
   const errorHandler = () => {
@@ -67,15 +84,17 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            onChange={usernameHandler}
-            value={enteredUserDetails.username}
+            // onChange={usernameHandler}
+            // value={enteredUserDetails.username}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            onChange={ageHandler}
-            value={enteredUserDetails.age}
+            // onChange={ageHandler}
+            // value={enteredUserDetails.age}
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
--- react_concepts/readme.md
+++ react_concepts/readme.md
@@ -13,7 +13,7 @@
 
 4- States
 1. events, event handlers
-2. useState hook
+2. useState hook (ALl React hooks works only inside functional components)
 3. Adding forms, listening to user inputs
 4. two way binding 
 5. working with multiple states (with object)
@@ -31,3 +31,18 @@
 1. check console errors
 2. browser breakpoints
 3. react dev tools
+
+7- Styling
+1. styled components
+2. css modules
+
+8- Fragments, portals, refs
+1. JSX component - should be wrapped in single element/ array(but with keys)/ fragments (preferred) (React Fragment logic - can create a component which returns props.children and use that also)
+2. Portals
+3. refs- helps in accessing other DOM elements & work with them.
+Mostly used in i/p elements, but can be used for any HTML element 
+we can get access to real DOM node (it is suggested that we don't manipulate DOM, we can read data though)
+For read-only refs are best, if e have a case to change values state is better
+4. state - controlled components (i/p in our case) as internal state is controlled by react
+5. refs - uncontrolled components (i/p in our case) as internal state is not controlled by react
+we are only using a regular DOM API to make changes in DOM node but with a react feature 'refs'
